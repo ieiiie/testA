8e5f32a30e537f4d5ccc6a5ea94460da29fd560a
xy <810346411@qq.com>
2019-08-04 16:31:15 +0800

    00000

--- conf/default.py
+++ conf/default.py
@@ -31,15 +31,15 @@ from django.conf.global_settings import *  # noqa
 # ==============================================================================
 # 在蓝鲸智云开发者中心 -> 点击应用ID -> 基本信息 中获取 APP_ID 和 APP_TOKEN 的值
 APP_ID = 'test'
-APP_TOKEN = '5cd901ca-2ddb-4b21-a38b-0ba664e9e102'
+APP_TOKEN = 'efc8641d-d948-4e79-8249-4bf779829df2'
 # 蓝鲸智云开发者中心的域名，形如：http://paas.bking.com
-BK_PAAS_HOST = 'http://http://paas.bk.com'
+BK_PAAS_HOST = 'http://paas.bk.com'
 
 # 请求官方 API 默认版本号，可选值为："v2" 或 ""；其中，"v2"表示规范化API，""表示未规范化API
 DEFAULT_BK_API_VER = 'v2'
 
 # 是否启用celery任务
-IS_USE_CELERY = True
+IS_USE_CELERY = False
 # 本地开发的 celery 的消息队列（RabbitMQ）信息
 BROKER_URL_DEV = 'amqp://guest:guest@127.0.0.1:5672/'
 # TOCHANGE 调用celery任务的文件路径, List of modules to import when celery starts.
